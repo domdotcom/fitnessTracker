b723c2c262f47014fd2f9cc392437b542b99a573
Dominika <dominikaperkowska@gmail.com>
2020-09-05 11:13:55 -0400

    added apiRoutes.js

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,44 @@
+var db = require("../models");
+
+module.exports = function (app) {
+  app.get("/api/workouts", function (req, res) {
+    db.Workout.find({})
+      .then(function (dbWorkout) {
+        res.json(dbWorkout);
+      })
+      .catch((err) => res.json(err));
+  });
+
+  app.put("/api/workouts/:id", function (req, res) {
+    console.log(req.body);
+    db.Workout.findOneAndUpdate(
+      { _id: req.params.id },
+      { $push: { exercises: req.body } },
+      { new: true }
+    ).then(function (dbWorkout) {
+      res.json(dbWorkout);
+    });
+  });
+
+  app.get("/api/workouts/range", (req, res) => {
+    console.log("range");
+    db.Workout.find()
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  app.post("/api/workouts", (req, res) => {
+    console.log(req.body);
+    db.Workout.create(req.body)
+      .then((dbExercise) => {
+        res.json(dbExercise);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+};
--- Develop/server.js
+++ Develop/server.js
@@ -14,3 +14,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useUnifiedTopology: true });
+
+require("./routes/apiRoutes.js")(app);
+require("./routes/htmlRoutes.js")(app);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
