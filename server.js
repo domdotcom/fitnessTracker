a06eff44548b7cefa05d5fd23cbc848cd17c4395
Dominika <dominikaperkowska@gmail.com>
2020-10-15 20:40:02 -0400

    changed routes

--- server.js
+++ server.js
@@ -22,8 +22,9 @@ mongoose.connect(
 
 require("./routes/htmlRoutes");
 
-app.use(require("./routes/htmlRoutes.js"));
 app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
